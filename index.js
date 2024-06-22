5dda5de6ca801567ca5765659af4814b8554e7b4
Aryan Jain <aryanrameshjain@gmail.com>
2024-06-23 01:03:59 +0530

    Complete basic portfolio project using html, css, js

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app=express();
+const port=3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.get("/projects",(req,res)=>{
+    res.render("projects.ejs");
+})
+
+app.listen(port,()=>{
+    console.log("Server is running")
+})
