4af60b93b45ad83c83402ee2f797781518926918
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-06-23 01:15:44 +0530

    Change port for render

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app=express();
-const port=3000;
+const port=10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",(req,res)=>{
